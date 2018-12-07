db905c0069aba6f0e7ce4237ba28846d936c9265
David <slamvsem@gmail.com>
2018-12-07 17:14:45 +0700

    adding news

--- api.nstu.news.py
+++ api.nstu.news.py
@@ -7,7 +7,6 @@ import getter
 import keyboards
 from datetime import datetime
 
-tags = ['<strong>', '</strong>', '<p>', '</p>', '</a>', '>', '\n']
 connection = data.connect()
 
 def auth():
@@ -19,7 +18,7 @@ def auth():
 
 def get_actual_url():
     time = datetime.now()
-    url = "https://api.ciu.nstu.ru/v1.0/news/schoolkids/"+str(time.year)+"/"+str(time.month)+"/"+str(time.day)#+"?json_indent=1&decode_unicode_escape=1"
+    url = "https://api.ciu.nstu.ru/v1.0/news/schoolkids/"+str(time.year)+"/"+str(time.month)+"/"+str(time.day)
     return url
 
 def get_html(url):
@@ -93,12 +92,4 @@ def main():
         print('Ссылка: ',item['URL'])
         print('Дата: ',item['NEWS_DATE'])
         print('Коротко: ',item['SHORTTEXT'])'''
-    
-    
-
-
-
-
-if __name__ == '__main__':
-    main()
 
--- bot.py
+++ bot.py
@@ -9,8 +9,7 @@ import requests
 import database as data
 import getter
 import keyboards
-
-
+import api.nstu.news as api
 def auth():
     token = getter.get_token()
     print(token)
@@ -22,6 +21,7 @@ def subscribe(type, id):
     if data.get_field(connection=connection, table_name="USERS",select_field = type, field="ID_VK", value=id)[0][0]==False:
         data.set_field(connection = connection, table_name = "USERS", ID_VK = id, field = type, value = "1")
         vk.method("messages.send", {"user_id": id, "message": "Теперь я буду отправлять тебе новости! Люблю это😍", 'keyboard': key['main_menu']})
+        api.main()
     else:
         data.set_field(connection = connection, table_name = "USERS", ID_VK = id, field = type, value = "0")
         vk.method("messages.send", {"user_id": id, "message": "Не хочешь, как хочешь...\nНо, если передумаешь, я всегда готов💪🏻", 'keyboard': key['main_menu']})
