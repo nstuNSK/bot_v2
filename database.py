7fc65973254a0164499d23e86485d386156ffb9d
David <slamvsem@gmail.com>
2018-12-16 23:53:25 +0700

    adding new search by spheres

--- database.py
+++ database.py
@@ -55,7 +55,7 @@ def set_complex_str_in_field(connection, table_name, ID_VK, field, value):
 
 def set_field(connection, table_name, ID_VK, field, value):
     with connection.cursor() as cursor:
-        sql = "UPDATE " + table_name + " SET " + field + " = " + str(value) + " WHERE ID_VK = %s"
+        sql = "UPDATE " + table_name + " SET " + field + " = " + str(value) + " WHERE ID = %s"
         r = cursor.execute(sql, (ID_VK))
         connection.commit()
 def get_fields(connection,table_name,select_field,field,value):
